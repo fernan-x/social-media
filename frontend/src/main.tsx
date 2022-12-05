ba0282c23057594d5399c4bab46247f17f31a60a
Fernandes Alves Fabien <fabien@code42.fr>
2022-12-05 07:47:14 +0100

    Added : Init MUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from "@emotion/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { theme } from "./services/theme/theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Button variant="contained">Test</Button>
+    </div>
+  );
+};
+
+export default Home;
